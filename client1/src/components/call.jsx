4ea29dddef02ac4f2b3349508a3d37b9daefc9d7
isharadata <isharadata@gmail.com>
2025-05-17 01:49:47 +0300

    fix props

--- client1/src/components/call.jsx
+++ client1/src/components/call.jsx
@@ -60,7 +60,7 @@ const Call = (props) => {
         <>
         <div className="call-card">
             <div className="info">
-                {props.startTime} {props.duration} <audio controls><source src={recordingSrc} type="audio/mp3" />
+                {props.startTime} {props.duration} {props.recordingSid} <audio controls><source src={recordingSrc} type="audio/mp3" />
           Your browser does not support the audio element.
         </audio>
             </div>
