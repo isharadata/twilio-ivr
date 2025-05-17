b1c905ace820426e89fb8f2aa3d756ab2596aea4
isharadata <isharadata@gmail.com>
2025-05-17 15:59:28 +0300

    add recording download url

--- client1/src/components/call.jsx
+++ client1/src/components/call.jsx
@@ -7,6 +7,8 @@ import AppContext from '../AppContext';
 const Call = (props) => {
 
     //const recordingSrc = `/recordings/${props.recordingSid}.mp3`;
+    const recordingPreview = `https://drive.google.com/file/d/${props.gdriveFileId}`;
+
     const recordingSrc = `https://drive.google.com/file/d/${props.gdriveFileId}/preview`;
 
     const bUrl = `${window.location.host}`;
@@ -61,7 +63,7 @@ const Call = (props) => {
         <>
         <div className="call-card">
             <div className="info">
-                {props.startTime} {props.duration} <a href={recordingSrc}>{props.recordingSid}</a> <iframe src={recordingSrc} onload="this.style.height=(this.contentWindow.document.body.scrollHeight+2)+'px';this.style.width=(this.contentWindow.document.body.scrollWidth+2)+'px';"></iframe>
+                {props.startTime} {props.duration} <a href={recordingSrc}>{props.recordingSid}</a> <iframe src={recordingPreview} onload="this.style.height=(this.contentWindow.document.body.scrollHeight+2)+'px';this.style.width=(this.contentWindow.document.body.scrollWidth+2)+'px';"></iframe>
             </div>
             <div className="actions">
 
