bf81f9d00c4be35c2a0763abc5a018a0814484af
isharadata <isharadata@gmail.com>
2025-05-22 00:28:36 +0300

    fix evaluate syntax

--- client1/src/components/calldialog/calldialog.jsx
+++ client1/src/components/calldialog/calldialog.jsx
@@ -23,7 +23,7 @@ socket.on("message", data => {
 
   console.log(`message: JSON.stringify(${parsedData}) ${eventType}`);
 
-  let flowParentDiv = document.evaluate('//DialogContent');
+  let flowParentDiv = document.evaluate('//DialogContent', document, null, XPathResult.ANY_TYPE, null);
   renderEvent(flowParentDiv, eventType, parsedData);
 
 });
