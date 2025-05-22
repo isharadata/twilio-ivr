e48190ed25f05c2d01be5bddad527f60874bb301
isharadata <isharadata@gmail.com>
2025-05-22 15:34:19 +0300

    fix AppContext path in socket

--- client1/src/socket.js
+++ client1/src/socket.js
@@ -1,5 +1,6 @@
 import { io } from 'socket.io-client';
-import AppContext from '../AppContext';
+
+import AppContext from './AppContext';
 
 // "undefined" means the URL will be computed from the `window.location` object
 //const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3001';
